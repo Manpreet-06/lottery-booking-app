7dbc5732f0e3e5c06d524b6842d64a07dbc0a684
Manpreet Arora <67434653+Manpreet-06@users.noreply.github.com>
2023-10-26 00:20:03 +0530

    Add: assets added

--- src/components/TicketCard/TicketCard.js
+++ src/components/TicketCard/TicketCard.js
@@ -1,21 +1,21 @@
-import { Card, Typography } from "@mui/material";
+import { Box, Card, Typography } from "@mui/material";
 import React from "react";
 
 const TicketCard = () => {
   const ticketArray = [
     {
       id: 1,
-      ticketNo: "RATLMISHOP1_GAME_3_A",
+      ticketNo: "RATLMISHOP1_GAME_3_A BCFY123457",
       price: "35000",
     },
     {
       id: 2,
-      ticketNo: "RATLMISHOP1_GAME_3_A",
+      ticketNo: "RATLMISHOP1_GAME_3_A BCFY123457",
       price: "25000",
     },
     {
       id: 3,
-      ticketNo: "RATLMISHOP1_GAME_3_A",
+      ticketNo: "RATLMISHOP1_GAME_3_A BCFY123457",
       price: "55000",
     },
   ];
@@ -23,10 +23,24 @@ const TicketCard = () => {
     <div>
       {ticketArray?.map((item) => {
         return (
-          <Card>
-            <Typography>Ticket Number:-</Typography>
-            <Typography>{item.ticketNo}</Typography>
-            <Typography>{item.price}</Typography>
+          <Card style={{ padding: "10px", color: "#0c3b5e", margin: '2px', boxShadow: "0px 2px 3px 2px lightgrey" }}>
+            <Typography
+              style={{ textAlign: "left", fontSize: "16px", fontWeight: 600 }}
+            >
+              TICKET NUMBER:-
+            </Typography>
+            <Box
+              display="flex"
+              alignItems={"center"}
+              justifyContent={"space-between"}
+            >
+              <Typography style={{ fontSize: "16px", fontWeight: 600, textAlign: "left" }}>
+                {item.ticketNo}
+              </Typography>
+              <Typography style={{ fontSize: "16px", fontWeight: 600,  textAlign: "left" }}>
+                {item.price}
+              </Typography>
+            </Box>
           </Card>
         );
       })}
